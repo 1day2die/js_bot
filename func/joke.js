aa64801aa91f41ab58079208981d026ccb798ab7
IceToast <social@icetoast.de>
2022-02-22 11:28:50 +0100

    fix: 🚑️ Logging

--- func/joke.js
+++ func/joke.js
@@ -1,36 +1,31 @@
-
 async function getJoke() {
-    return new Promise((resolve) => {
-        var url = 'https://v2.jokeapi.dev/joke/Dark,Pun';
-        const https = require('https')
-        let retval;
+  return new Promise((resolve) => {
+    var url = "https://v2.jokeapi.dev/joke/Dark,Pun"
+    const https = require("https")
+    let retval
 
-        https.get(url, (res) => {
-            let body = "";
+    https
+      .get(url, (res) => {
+        let body = ""
 
-            res.on("data", (chunk) => {
-                body += chunk;
-            });
+        res.on("data", (chunk) => {
+          body += chunk
+        })
 
-            res.on("end", () => {
-                try {
-                        let json = JSON.parse(body);
-                        retval = (json.setup + " || " + json.delivery + "||");
-                } catch (error) {
-                    console.error(error.message);
-                }
-                ;
-            });
-
-        }).on("error", (error) => {
-            console.error(error.message);
-        });
-        resolve(retval);
-    });
-}
-async function jokeOutput() {
-    const ans = getJoke();
-    console.log(ans);
+        res.on("end", () => {
+          try {
+            let json = JSON.parse(body)
+            retval = json.setup + " || " + json.delivery + "||"
+          } catch (error) {
+            console.error(error.message)
+          }
+        })
+      })
+      .on("error", (error) => {
+        console.error(error.message)
+      })
+    resolve(retval)
+  })
 }
 
-module.exports = { getJoke, jokeOutput };
+module.exports = { getJoke }
